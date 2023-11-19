b0db0f31e2610c052fb9b748ff624d8c4ad2c80b
alexbrown <alexander_brown_@hotmail.com>
2023-11-19 02:40:48 +0000

    style changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,110 @@
-import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
-import { connectWebSocket, sendMessage, updateConnectButtonState } from './chat';
+import { sendMessage } from './chat';
+import { refreshOnlineUsersList } from './chat';
+
 function App() {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  updateConnectButtonState();
+    const [username, setUsername] = useState('');
+    const [message, setMessage] = useState('');
+    const [webSocket, setWebSocket] = useState(null);
+    const [isConnected, setIsConnected] = useState(false);
+    const [onlineUsers, setOnlineUsers] = useState([]);
 
-  return (
-    <div className="App">
-    <div className="chat-box" id='chatbox'>
-    </div>
-    <div className="messageInput">
-      <textarea 
-        value={message}
-        id={'message-input'}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder="Type your message here..."
-      />
-      <button onClick={sendMessage}>Send</button>
-    </div>
-    <div className="username-input">
-      <input 
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Enter username"
-      />
-      <button id= 'connectButton'onClick={() => connectWebSocket(username)}>Connect</button>
 
-    </div>
-  </div>
-  );
+    const connectWebSocket = () => {
+        const ws = new WebSocket(`wss://behktal0d3.execute-api.eu-west-2.amazonaws.com/dev/?username=${encodeURIComponent(username)}`);
+
+        ws.onopen = () => setIsConnected(true);
+        ws.onclose = () => setIsConnected(false);
+        ws.onerror = (error) => {
+            console.error('WebSocket Error:', error);
+            setIsConnected(false);
+        };
+        ws.onmessage = (event) => {
+          const data = JSON.parse(event.data);
+          console.log(event)
+          if (data.action === 'userConnected') {
+              setOnlineUsers((prevUsers) => [...prevUsers, data.username]);
+          } else if (data.action === 'userDisconnected') {
+              setOnlineUsers((prevUsers) => prevUsers.filter(user => user !== data.username));
+          }
+          else if (data.action === 'onlineUsersList') {
+            updateOnlineUsersList(data.users); // Implement this function
+        }  
+          else {
+              // Handle regular chat messages
+              const chatbox = document.getElementById('chatbox');
+              const messageElement = document.createElement('div');
+              messageElement.textContent = `${data.sender}: ${data.message}`;
+              chatbox.appendChild(messageElement);
+          }
+      };
+      
+
+        setWebSocket(ws);
+    };
+
+    const sendMessage_= () => {
+        if (message && webSocket) {
+            sendMessage(webSocket, message, username);
+            setMessage('');
+        }
+    };
+
+    const handleDisconnect = () => {
+        if (webSocket) {
+            webSocket.close();
+        }
+        setIsConnected(false);
+    };
+
+    function updateOnlineUsersList(users) {
+      setOnlineUsers(users)
+  } 
+
+  function refresh() {
+    if (webSocket) {
+      refreshOnlineUsersList(webSocket);
+    }
+  }
+
+    return (
+      <div className="App">
+            <div className="chat-section">
+                <div className="chat-box" id='chatbox'>
+                    {/* Chat messages */}
+                </div>
+                <div className="messageInput">
+                    <textarea 
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder="Type your message here..."
+                        disabled={!isConnected}
+                    />
+                    <button onClick={sendMessage_} disabled={!isConnected}>Send</button>
+                </div>
+                <div className="username-input">
+                    <input 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Enter username"
+                        disabled={isConnected}
+                    />
+                    <button onClick={connectWebSocket} disabled={isConnected}>Connect</button>
+                    <button onClick={handleDisconnect} disabled={!isConnected}>Disconnect</button>
+                </div>
+            </div>
+            <div className="online-users">
+              <button onClick={refresh}>Refresh Users</button>
+                <h3>Online Users</h3>
+                <ul id='online-users-list'>
+                    {onlineUsers.map((user, index) => (
+                        <li key={index}>{user}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/chat.js
+++ src/chat.js
@@ -1,92 +1,20 @@
 let webSocket
 
-
-let userName = ''
-
-function connectWebSocket(username) {
-    // const username = document.getElementById('username-input').value;
-    if (!username) {
-        alert('Please enter a username.');
-        return;
-    }
-    userName = username
-    // Construct the WebSocket URL with the username as a query parameter
-    const webSocketUrl = `wss://behktal0d3.execute-api.eu-west-2.amazonaws.com/dev/?username=${encodeURIComponent(username)}`;
-    webSocket = new WebSocket(webSocketUrl);
-
-    webSocket.onopen = function(event) {
-        console.log('Connected to WebSocket');  
-        updateConnectButtonState()
-        // Additional logic upon successful connection
-    };
-
-
-    webSocket.onmessage = function(event) {
-        console.log('Message received: ', event.data);
-        const data = JSON.parse(event.data);
-    
-        const chatbox = document.getElementById('chatbox');
-        const messageElement = document.createElement('div');
-        messageElement.textContent = ` ${data.sender}: ${data.message}`;
-        chatbox.appendChild(messageElement);
-    };
-
-    webSocket.onerror = function(event) {
-        console.error('WebSocket error: ', event);
-        // Handle WebSocket errors
-    };
-
-    webSocket.onclose = function(event) {
-        console.log('WebSocket connection closed: ', event);
-        updateConnectButtonState()
-        // Handle WebSocket connection closure
-    };
-}
-
-function sendMessage() {
-    const messageInput = document.getElementById('message-input');
-    const message = messageInput.value;
-    messageInput.value = ''; // Clear the input field
-
-    if (!message) {
-        alert("Empty message");
-    }
-    else if (webSocket && webSocket.readyState === WebSocket.OPEN) {
-        const data = JSON.stringify({ action: 'sendMessage', message: message , username:userName});
+function sendMessage(webSocket, message, username) {
+    if (webSocket && webSocket.readyState === WebSocket.OPEN) {
+        const data = JSON.stringify({ action: 'sendMessage', message, username });
         webSocket.send(data);
     } else {
         console.error('WebSocket is not connected.');
-        if (webSocket) {console.log(webSocket.readyState)}
-        document.getElementById('connectButton').disabled = false;
-        document.getElementById('connectButton').style.background = 'red';
-
-        alert('Please connect')
     }
 }
 
-function updateConnectButtonState() {
-    const connectButton = document.getElementById('connectButton');
-    if (!connectButton) return
+function refreshOnlineUsersList(webSocket) {
     if (webSocket && webSocket.readyState === WebSocket.OPEN) {
-        connectButton.disabled = true; // Disable the button if WebSocket is open
-        connectButton.style.background = 'green'
-    } else {
-        connectButton.disabled = false; // Enable the button otherwise
-        connectButton.style.background = 'red'
-
+        webSocket.send(JSON.stringify({ action: 'getOnlineUsers' }));
     }
 }
 
-window.addEventListener('beforeunload', function() {
-    if (webSocket && webSocket.readyState === WebSocket.OPEN) {
-        webSocket.close();
-    }
-});
-
-window.addEventListener('load', function() {
-    updateConnectButtonState();
-    // Any additional initialization logic for WebSocket connection
-});
 
 
-export {connectWebSocket, sendMessage, updateConnectButtonState};
+export {sendMessage, refreshOnlineUsersList};
